e50ab5e842b9e29678fa82653dfcaebad5ae566e
Travel987 <brianapowers78@gmail.com>
2026-08-26 18:52:51 -0500

    Set up Spring Boot project and database configuration

---
+++ src/main/java/com/brianapowers/repandrepeat/RepAndRepeatApplication.java
@@ -0,0 +1,13 @@
+package com.brianapowers.repandrepeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepAndRepeatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepAndRepeatApplication.class, args);
+    }
+
+}
